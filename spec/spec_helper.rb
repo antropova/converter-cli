ad6ad7a7513b1e771e85dcc606a4877bc093503e
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 18:29:19 -0400

    update spec syntax from metadata config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,10 @@ require_relative '../config/environment'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'default'
-  config.before(:each) do
-    #hint - in here you can run all kinds of things
-  end
 end
  
