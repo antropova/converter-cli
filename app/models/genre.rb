9e4cf5e9b4698cd0b129af016b428b6675099b1f
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 18:48:39 -0400

    update cli to have app/ file structure

---
+++ app/models/genre.rb
@@ -0,0 +1,3 @@
+class Genre
+  # code here
+end
--- spec/library_parser_spec.rb
+++ spec/library_parser_spec.rb
@@ -1,17 +1,24 @@
 require_relative 'spec_helper.rb'
 
 describe "LibraryParser" do
-  before(:each) do
+  before do
     Artist.reset_all
     Song.reset_all
     Genre.reset_all
   end
 
-  let(:parser){LibraryParser.new('spec/fixtures/mp3s')}
+  let(:parser){ LibraryParser.new }
+
+  it 'will parse the library' do
+    LibraryParser.parse
+    expect(Artist.all).to_not be_empty
+    expect(Genre.all).to_not be_empty
+    expect(Song.all).to_not be_empty
+  end
 
   it 'loads files from a directory' do
     expect(parser.files).to_not be_empty
-    expect(parser.files.size).to eq(1)
+    expect(parser.files.size).to eq(99)
   end
 
   it 'parses a filename into 3 parts' do
@@ -23,7 +30,7 @@ describe "LibraryParser" do
 
   it 'builds a song based on song parts' do
     parts = ['Action Bronson', 'Larry Csonka', 'indie']
-    song = parser.build_song(parts[0], parts[1], parts[2])
+    song = parser.build_song(*parts)
 
     expect(Artist.find_by_name(parts[0])).to eq(song.artist)
     expect(Song.find_by_name(parts[1])).to eq(song)
@@ -31,7 +38,7 @@ describe "LibraryParser" do
   end
 
   it 'will parse all songs in a directory' do
-    parser.parse
+    parser.call
 
     expect(Artist.all).to_not be_empty
     expect(Genre.all).to_not be_empty
