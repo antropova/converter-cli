05abb81477d54e9b124cd79613efa026df93954a
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-02-23 17:12:16 -0500

    Add working converter

--- app/runners/converter_cli.rb
+++ app/runners/converter_cli.rb
@@ -1,8 +1,13 @@
+# require 'json'
+# require 'pry'
+
 class ConverterCLI
 
   def initialize
-    latest_url = "https://openexchangerates.org/api/latest.json?app_id=3fb2b129f6b64345a150612f6362b1fb"
-    @rates = JSON.load(open(latest_url))
+    # latest_url = "https://openexchangerates.org/api/latest.json?app_id=3fb2b129f6b64345a150612f6362b1fb"
+    # @rates = JSON.load(open(latest_url))
+    json_rates = JSON.parse( IO.read("./spec/fixtures/rates.json") )
+    @rates = json_rates["rates"]
     @countries = JSON.parse( IO.read("./spec/fixtures/currency_country_names.json") )
   end
 
@@ -12,41 +17,49 @@ class ConverterCLI
     run
   end
     
-  def get_user_input
-    gets.chomp.strip
-  end
-
   def run
-    print "New search keyword: "
+    puts "Insert currency name"
     input = get_user_input
-    if input == "help"
-      help
-    elsif input == "exit"
-      exit
-    else
-      search(input)
-    end
+    parse(input)
     run
   end
-
-  def search(input)
-    # search_term = input.split(" ")[0]
-    # puts "Your search term was #{search_term}, I am searching..."
-    # url = "https://twitter.com/search?q=#{search_term}&src=typd&f=realtime"
-    # tweet = ExampleScraper.new(url).example_method.sample
-    # puts "Thank you for your patience. I found this on Twitter:"
-    # puts tweet.example
+  
+  def get_user_input
+    gets.chomp
+  end
+ 
+  def parse(input)
+    if @rates.keys.include?(input)
+      print_rate(input)
+    elsif input =~ /to\s\w{3}\$/
+      do_math(input)
+    elsif input =~ /^\d+/
+      do_math(input)
+    end
   end
 
-  def help
-    puts "Type 'exit' to exit"
-    puts "Type 'help' to view this menu again"
-    puts "Type 'list' to list currencies shortcuts (or countries)"
-        # this will lead to another user input that's going to
-        # prompt user to type in the country (match it with regex)
-        # and give a short description of the country (wikipedia/)
-    puts "Type a country name to search by country"
-    # puts "Type anything else to search for a Tweet"
+  def print_rate(input)
+    puts "1 #{input} is #{1/@rates[input]} USD"
+    puts "1 USD is #{@rates[input]} #{input}"
+  end
+
+  def do_math(input)
+    input_array = input.split(" ")
+    if input_array.size == 2
+      convert = input_array[0].to_i / @rates[input_array[1]]
+      puts "#{input_array[0]} #{input_array[1]} is #{convert.round(2)} #{input_array[2]} USD"
+    elsif input_array.size == 4
+      usd_amount = input_array[0].to_i / @rates[input_array[1]]
+      cur_amount = usd_amount * @rates[input_array[3]]
+      puts "#{input_array[0]} #{input_array[1]} is #{cur_amount.round(2)} #{input_array[3]}"
+    else
+      puts "Error!"
+    end
+  end
+  
+  def print_result
+    puts 'rate'
   end
 
 end
+
