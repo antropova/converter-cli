60f0ef31eeac5d45946da1f2e7b937ba7d2beba1
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-02-24 17:53:01 -0500

    Add case 3 to parser module

--- app/concerns/parser.rb
+++ app/concerns/parser.rb
@@ -3,10 +3,17 @@ module Parser
     case_1 = /^\w{3}$/
     case_2 = /^\d+\s\w{3}$/
     case_3 = /^\d+\s\w{3}\sto\s\w{3}$/i
-    if input =~ case_1 && rates.countries.keys.include?(input.upcase)
+    case_4 = /^\d+\s\w{3}\sto\s\w{3}\s\d{4}-\d{2}-\d{2}$/i
+    if input =~ case_1 && api.countries.keys.include?(input.upcase)
       amount = 1
       initial_cur = input
       result_cur = "usd"
+    elsif input =~ case_4
+      input_array = input.split(" ") 
+      amount = input_array[0]
+      initial_cur = input_array[1]
+      input_array[3] ? result_cur = input_array[3] : result_cur = 'usd'
+      date = input_array[4]
     elsif input =~ case_2 || input =~ case_3
       input_array = input.split(' ')
       amount = input_array[0]
@@ -16,6 +23,6 @@ module Parser
       puts Rainbow("Some error occured, please try again").red.bright
       run
     end
-    [amount.to_i, initial_cur.upcase, result_cur.upcase]
+    [amount.to_i, initial_cur.upcase, result_cur.upcase, date]
   end  
 end
