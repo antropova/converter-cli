93a32f1b688c0cf8fd1166715d14079dd6e2cc05
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-02-24 14:07:02 -0500

    Add working api version

--- app/models/rates.rb
+++ app/models/rates.rb
@@ -2,10 +2,10 @@ class Rates # json object
   attr_reader :rates, :countries
 
   def initialize
-    json_rates = JSON.parse( IO.read("./spec/fixtures/rates.json") )
+    # json_rates = JSON.parse( IO.read("./spec/fixtures/rates.json") )
+    latest_url = "https://openexchangerates.org/api/latest.json?app_id=3fb2b129f6b64345a150612f6362b1fb"
+    json_rates = JSON.load(open(latest_url))
     @rates = json_rates["rates"]
     @countries = JSON.parse( IO.read("./spec/fixtures/currency_country_names.json") )
-    # latest_url = "https://openexchangerates.org/api/latest.json?app_id=3fb2b129f6b64345a150612f6362b1fb"
-    # json_rates = JSON.load(open(latest_url))
   end
 end
