e26463be96b4ba4bce277f184268b01f2160e6e3
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-02-26 15:54:14 -0500

    Refactored parser module with Steven

--- app/concerns/parser.rb
+++ app/concerns/parser.rb
@@ -1,28 +1,87 @@
 module Parser
   def parse(input)
-    case_1 = /^\w{3}$/
-    case_2 = /^\d+\s\w{3}$/
-    case_3 = /^\d+\s\w{3}\sto\s\w{3}$/i
-    case_4 = /^\d+\s\w{3}\sto\s\w{3}\s\d{4}-\d{2}-\d{2}$/i
-    if input =~ case_1 && api.countries.keys.include?(input.upcase)
-      amount = 1
-      initial_cur = input
-      result_cur = "usd"
-    elsif input =~ case_4
-      input_array = input.split(" ") 
-      amount = input_array[0]
-      initial_cur = input_array[1]
-      input_array[3] ? result_cur = input_array[3] : result_cur = 'usd'
-      date = input_array[4]
-    elsif input =~ case_2 || input =~ case_3
-      input_array = input.split(' ')
-      amount = input_array[0]
-      initial_cur = input_array[1]
-      input_array[3] ? result_cur = input_array[3] : result_cur = 'usd'
-    else 
-      puts Rainbow("Some error occured, please try again").red.bright
-      run
-    end
-    [amount.to_i, initial_cur.upcase, result_cur.upcase, date]
+    CurrencyConverter.new(input).call
+  rescue CurrencyNotFound
+    puts Rainbow("Some error occured, please try again").red.bright
+    run
   end  
+end
+
+class CurrencyNotFound < StandardError; end
+
+class CurrencyConverter
+  def self.new(input)
+    case input
+    when /^\w{3}$/
+      FromCountryCode.new(input)
+    when /^\d+\s\w{3}$/
+      AmountWithCurrency.new(input)
+    when /^\d+\s\w{3}\sto\s\w{3}$/i
+      AmountWithCurrencyAndConversion.new(input)
+    when /^\d+\s\w{3}\sto\s\w{3}\s\d{4}-\d{2}-\d{2}$/i 
+      WithAmountAndDate.new(input)
+    else
+      raise CurrencyNotFound
+    end
+  end
+
+end
+
+class AmountWithCurrency
+  attr_reader :input, :default_currency
+  def initialize(input)
+    @input = input
+    @default_currency = "USD"
+  end
+
+  def call
+    input_array = input.split(' ')
+    amount = input_array[0]
+    initial_cur = input_array[1]
+    [amount.to_i, initial_cur.upcase, default_currency]
+  end
+end 
+
+class AmountWithCurrencyAndConversion
+  attr_reader :input
+  def initialize(input)
+    @input = input
+  end
+
+  def call
+    input_array = input.split(" ") 
+    amount = input_array[0]
+    initial_cur = input_array[1]
+    result_cur = input_array[3] 
+    [amount.to_i, initial_cur.upcase, result_cur.upcase]
+  end
+end 
+
+class WithAmountAndDate
+ attr_reader :input
+ def initialize(input)
+  @input = input
+  end
+
+  def call
+    input_array = input.split(" ") 
+    amount = input_array[0]
+    initial_cur = input_array[1]
+    result_cur = input_array[3]
+    date = input_array[4]
+    [amount.to_i, initial_cur.upcase, result_cur.upcase, date]
+  end
+end
+
+class FromCountryCode
+  attr_reader :input, :default_currency
+  def initialize(input)
+    @input = input
+    @default_currency = "USD"
+  end
+
+  def call
+    amount = 1
+    [amount.to_i, input.upcase, default_currency]
+  end
 end
